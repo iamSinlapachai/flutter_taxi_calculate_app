99c4b38fbf0d7240bf73413026687285984420c4
iamSinlapachai <s6752d10003@sau.ac.th>
2025-05-23 23:53:29 +0700

    Foundation

---
+++ lib/views/taxi_home_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaxiHomeUI extends StatefulWidget {
+  const TaxiHomeUI({super.key});
+
+  @override
+  State<TaxiHomeUI> createState() => _TaxiHomeUIState();
+}
+
+class _TaxiHomeUIState extends State<TaxiHomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
---
+++ lib/views/taxi_result_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaxiResultUI extends StatefulWidget {
+  const TaxiResultUI({super.key});
+
+  @override
+  State<TaxiResultUI> createState() => _TaxiResultUIState();
+}
+
+class _TaxiResultUIState extends State<TaxiResultUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
